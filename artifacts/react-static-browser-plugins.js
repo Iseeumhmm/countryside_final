bd4a27b3bc48d23233ffec3600beda2369e8459a
Iseeumhmm <rickheffren@Ricks-MacBook-Pro.local>
2020-02-10 11:31:30 -0500

    conflict

--- artifacts/react-static-browser-plugins.js
+++ artifacts/react-static-browser-plugins.js
@@ -1,23 +1,8 @@
 // Imports
-import plugin0 from '/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/node_modules/react-static-plugin-reach-router/browser.api.js'
+
 
 // Plugins
 const plugins = [{
-        location: "/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/node_modules/react-static-plugin-source-filesystem",
-        plugins: [],
-        hooks: {}
-      },
-{
-        location: "/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/node_modules/react-static-plugin-reach-router",
-        plugins: [],
-        hooks: plugin0({})
-      },
-{
-        location: "/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/node_modules/react-static-plugin-sitemap/dist",
-        plugins: [],
-        hooks: {}
-      },
-{
         location: "/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting",
         plugins: [],
         hooks: {}
