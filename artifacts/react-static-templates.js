1243991bd13e1ffc8c9e24e26a26451143a21c10
Iseeumhmm <rickheffren@Ricks-MacBook-Pro.local>
2020-02-12 14:45:36 -0500

    added analytics

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -3,8 +3,14 @@
   
 // Template Map
 export default {
-  '/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/node_modules/react-static/lib/browser/components/Default404': require('/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/node_modules/react-static/lib/browser/components/Default404').default
+  '/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/src/pages/404.js': require('/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/src/pages/404.js').default,
+'/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/src/pages/about.js': require('/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/src/pages/about.js').default,
+'/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/src/pages/contact.js': require('/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/src/pages/contact.js').default,
+'/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/src/pages/index.js': require('/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/src/pages/index.js').default,
+'/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/src/pages/services.js': require('/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/src/pages/services.js').default,
+'/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/src/pages/warranty.js': require('/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/src/pages/warranty.js').default,
+'/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/src/pages/pools.js': require('/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/src/pages/pools.js').default
 }
 
-export const notFoundTemplate = '/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/node_modules/react-static/lib/browser/components/Default404'
+export const notFoundTemplate = '/Users/rickheffren/Documents/WebDevelopment/CountrysideTesting/src/pages/404.js'
 
