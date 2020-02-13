3cac32dfc4c3e06248d04a26c31cfbeb18102f44
Iseeumhmm <rickheffren@Ricks-MacBook-Pro.local>
2020-02-12 22:08:24 -0500

    minor fixes pushing

--- src/components/helperFuncrtions/AnalyticsEvents.js
+++ src/components/helperFuncrtions/AnalyticsEvents.js
@@ -1,11 +1,11 @@
 import ReactGA from "react-ga";
 
 const Event = ( catagory ) => {
-    // ReactGA.event({
-    //     category:catagory,
-    //     action: "Clicked"
-    // })
-    console.log('GA - ', catagory)
+    ReactGA.event({
+        category:catagory,
+        action: "Clicked"
+    })
+    // console.log('GA - ', catagory)
 }
 
 export default Event
